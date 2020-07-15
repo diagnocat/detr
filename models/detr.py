corrected loss masking

models/detr.py
@@ -20,6 +20,7 @@ from .transformer import build_transformer
 
 class DETR(nn.Module):
     """ This is the DETR module that performs object detection """
+
     def __init__(self, backbone, transformer, num_classes, num_queries, aux_loss=False):
         """ Initializes the model.
         Parameters:
@@ -86,6 +87,7 @@ class SetCriterion(nn.Module):
         1) we compute hungarian assignment between ground truth boxes and the outputs of the model
         2) we supervise each pair of matched ground-truth / prediction (supervise class and box)
     """
+
     def __init__(self, num_classes, matcher, weight_dict, eos_coef, losses):
         """ Create the criterion.
         Parameters:
@@ -121,9 +123,11 @@ class SetCriterion(nn.Module):
         # LVIS-compatible masking using not_exhaustive_category_ids
         predicted = src_logits.argmax(2)
         keep = []
-        for pr, t in zip(predicted, targets):
+        for pr, t, gt in zip(predicted, targets, target_classes):
             undef_cat = t['not_exhaustive_category_ids']
-            keep.append(~((pr[None].expand(len(undef_cat), -1) == undef_cat[:, None]).any(0)))
+            predicted_category_undef = (pr[None].expand(len(undef_cat), -1) == undef_cat[:, None]).any(0)
+            gt_foreground = gt != self.num_classes
+            keep.append((~predicted_category_undef) | gt_foreground)
         keep = torch.stack(keep, 0)
         per_element_weight = self.empty_weight[target_classes]
         loss_ce = F.cross_entropy(src_logits.transpose(1, 2), target_classes, self.empty_weight, reduction='none')